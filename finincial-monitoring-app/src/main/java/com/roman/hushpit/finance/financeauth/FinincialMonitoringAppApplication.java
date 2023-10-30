8b1c2eaf50026502c65ed5b272a6ef241353240d
Roman Hushpit <roman.gushpit@gmail.com>
2023-10-30 19:59:27 +0200

    Initialize app

---
+++ finincial-monitoring-app/src/main/java/com/roman/hushpit/finance/financeauth/FinincialMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.roman.hushpit.finance.financeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinincialMonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinincialMonitoringAppApplication.class, args);
+	}
+
+}
---
+++ finincial-monitoring-app/src/test/java/com/roman/hushpit/finance/financeauth/FinincialMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.hushpit.finance.financeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinincialMonitoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
